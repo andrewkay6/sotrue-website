d6efb0d6a972bd07be58033f463f8c34aa4256f9
Andrew Kay <kaya6@mcmaster.ca>
2024-07-03 14:47:08 -0400

    Fixed mobile screen issues

--- src/FeaturedVideo.tsx
+++ src/FeaturedVideo.tsx
@@ -15,7 +15,14 @@ const FeaturedVideo = () => {
         <CardMedia src="https://www.youtube.com/embed/WNs3AmnJGjE?si=36nN4UZ8mcyR-A7z"></CardMedia>
       </Card> */}
 
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/0dgRMRW9t_c?si=VAkr9f1cO6bCGGIe" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <iframe width="100%" 
+      height="100%" 
+      src="https://www.youtube.com/embed/0dgRMRW9t_c?si=VAkr9f1cO6bCGGIe" 
+      title="YouTube video player" 
+      frameBorder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      referrerPolicy="strict-origin-when-cross-origin" 
+      allowFullScreen></iframe>
     </Box>
   );
 };
