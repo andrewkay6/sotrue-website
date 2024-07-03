4edd1c283274b597e4fac90b5de70671dd150cbe
Andrew Kay <kaya6@mcmaster.ca>
2024-07-03 12:49:51 -0400

    Fixed build issues

--- src/FeaturedVideo.tsx
+++ src/FeaturedVideo.tsx
@@ -6,7 +6,7 @@ const FeaturedVideo = () => {
       height: "50%",
       width: "60%",
       maxWidth: "500px",
-      
+
     },
   };
   return (
@@ -15,16 +15,7 @@ const FeaturedVideo = () => {
         <CardMedia src="https://www.youtube.com/embed/WNs3AmnJGjE?si=36nN4UZ8mcyR-A7z"></CardMedia>
       </Card> */}
 
-      <iframe
-        width="100%"
-        height="100%"
-        src="https://www.youtube.com/embed/WNs3AmnJGjE?si=36nN4UZ8mcyR-A7z"
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        frameBorder={0}
-        allowFullScreen
-      />
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/0dgRMRW9t_c?si=VAkr9f1cO6bCGGIe" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </Box>
   );
 };
