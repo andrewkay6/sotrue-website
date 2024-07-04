3f064da012acc38d073e6fdda744e4f7515bc3ed
Andrew Kay <kaya6@mcmaster.ca>
2024-07-04 14:11:23 -0400

    Seeking with the audio player works now

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -11,6 +11,7 @@ const ProgressBar = (props : ProgressBarProps) => {
     <Box sx={{height: "100%", width: "100%", display: "flex", flexDirection: "row", justifyContent: "center"}}>
       <Slider 
         value={props.progressPercentage} 
+        onChange={(event, newValue) => {props.setProgressPercentage(newValue as number)}}
         size="small"
         aria-label="Progress"
         sx={{width: '98%'}}
